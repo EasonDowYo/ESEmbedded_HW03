ef7bc8bd63178ce208288768a8c468c1ad28e954
EasonDowYo <king0906sk@gmail.com>
2019-03-18 21:59:01 +0800

    HW03

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+
+
+int OneArgAdd(int a,int b){return a+b;}
+int MultiArgAdd(int a,int b,int c,int d,int e){return a+b+c+d+e;}
 void reset_handler(void)
 {
-	while (1)
-		;
+    int a=1,b=2,c,d,e,f;
+    c=OneArgAdd(a,b);
+    d=OneArgAdd(c,10);
+    e=20;
+    f=MultiArgAdd(a,b,c,d,e);
+    while(1);
+		
+        
+
 }
